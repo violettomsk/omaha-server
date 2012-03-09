4df44d8bc9952351c5fb63d5e08169f2e3c41886
Viatcheslav Gachkaylo <vgachkaylo@crystalnix.com>
2012-03-09 21:06:18 +0700

    Uncensor proxy output format modification.

--- uncensorp_out.py
+++ uncensorp_out.py
@@ -4,6 +4,7 @@ from config import Config
 import json
 from twisted.web.client import getPage
 from twisted.web import server
+import pycountry
 
 
 class UncensorPOutResource(resource.Resource):
@@ -11,7 +12,7 @@ class UncensorPOutResource(resource.Resource):
   pathFromRoot = '/service/uncensorp_domains'
 
   def render_GET(self, request):
-    d = getPage('http://api.hostip.info/country.php?ip=' + request.getClientIP())
+    d = getPage('http://api.hostip.info/country.php?ip=' + request.getHeader('x-forwarded-for'))
     d.addCallback(self.output_json, request)
     d.addErrback(self.errback, request)
     return server.NOT_DONE_YET
@@ -19,6 +20,8 @@ class UncensorPOutResource(resource.Resource):
   def output_json(self, value, request):
     macdb = MacDbHelper()
     res = macdb.uncensorp_fetch_by_iso(value)
+    res = { 'domains': [rec['domain'] for rec in res], 'country_code': value,
+            'country_name': pycountry.countries.get(alpha2=row['iso']).name.encode('utf-8') }
     macdb.cleanup()
 
     request.setHeader('Content-Type', 'application/json')
