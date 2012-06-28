db6ed595b65333632964d2d948e7b5d77d548c56
Viatcheslav Gachkaylo <vgachkaylo@crystalnix.com>
2012-06-28 18:00:57 +0700

    More fixes to stats gathering.

--- uncensor_out.py
+++ uncensor_out.py
@@ -1,4 +1,5 @@
 from twisted.web import resource
+from twisted.python import log
 from db_helper import DbHelper
 from config import Config
 import json, re
@@ -13,13 +14,20 @@ class UncensorOutResource(resource.Resource):
     ua = request.requestHeaders.getRawHeaders('User-Agent')
     if ua != None:
       ua = ua[0]
-      x2 = ua.split()[1]
-      os = 'win' if (x2.find('Windows') != -1) else 'mac' if (x2.find('Mac OS') != -1) else None
+      matchObj = re.match( r'.*? (\(.*?\)) .*', ua)
+      os = None
+      version = None
 
-      matchObj = re.match( r'BitPop/(\d+\.\d+\.\d+\.\d+)', ua) # get version
+      if matchObj:
+        x2 = matchObj.group(1)
+        os = 'win' if (x2.find('Windows') != -1) else 'mac' if (x2.find('Mac OS') != -1) else None
 
-      if matchObj and os:
-        macdb.stats_add(matchObj.groups(1), os)
+      matchObj = re.match( r'.* BitPop/(\d+\.\d+\.\d+\.\d+) .*', ua) # get version
+      if matchObj:
+        version = matchObj.group(1)
+
+      if version and os:
+        macdb.stats_add(version, os)
 
     res = macdb.uncensor_fetch_all()
     macdb.cleanup()
